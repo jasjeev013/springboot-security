1d512bf984cbffec8be94e14f030d70ee347cff0
Jasjeev <jasjeev99@gmail.com>
2024-07-19 00:19:52 +0530

    Updated README.md. Started with basics and working

---
+++ springsection1/README.md
@@ -0,0 +1,6 @@
+1. Building Spring Project
+2. Implementing Normal Spring Security 
+3. Added customer user ^ password
+4. Learnt Implementation of SecurityProperties.class (By default Security Implementing class)
+5. Understood Operations team security and development phase security (Video 7)
+6. Spring Security Internal Flow
---
+++ springsection1/src/main/java/com/eazybytes/springsection1/controllers/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springsection1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "Welcome to spring";
+    }
+}
